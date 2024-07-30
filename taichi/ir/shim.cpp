fcb32c952d91eb674f4ada27ef9bb4f6dc7f980f
Deadbeef <ent3rm4n@gmail.com>
2024-07-30 19:45:08 +0800

    +1

--- taichi/ir/shim.cpp
+++ taichi/ir/shim.cpp
@@ -4,4 +4,15 @@ namespace taichi::lang {
     std::unique_ptr<IRBuilder> ir_builder_new() {
         return std::make_unique<IRBuilder>();
     }
+    AllocaStmt* ir_builder_create_local_var(IRBuilder* builder, PrimTy ty) {
+        DataType d;
+        switch(ty) {
+            #define PER_TYPE(x) case PrimTy::x:\
+                d = PrimitiveType::x;\
+                break;
+            #include "taichi/inc/data_type.inc.h"
+            #undef PER_TYPE
+        }
+        return builder->create_local_var(d);
+    }
 }
--- taichi/ir/shim.h
+++ taichi/ir/shim.h
@@ -2,4 +2,11 @@
 
 namespace taichi::lang {
     std::unique_ptr<IRBuilder> ir_builder_new();
+    enum class PrimTy: uint16_t {
+        #define PER_TYPE(x) x,
+        #include "taichi/inc/data_type.inc.h"
+        #undef PER_TYPE
+    };
+    AllocaStmt* ir_builder_create_local_var(IRBuilder* builder, PrimTy ty);
 }
+
