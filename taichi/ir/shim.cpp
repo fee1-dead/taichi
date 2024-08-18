5f0814779f780f41ddeeb31b8709476855e35fa3
makai410 <glut410@outlook.com>
2024-08-18 22:02:28 +0800

    manual

--- taichi/ir/shim.cpp
+++ taichi/ir/shim.cpp
@@ -1,4 +1,6 @@
 #include "taichi/ir/shim.h"
+#include "taichi/ir/ir_builder.h"
+#include "taichi/ir/statements.h"
 
 namespace taichi::lang {
     std::unique_ptr<Kernel> new_kernel(Program &program,
@@ -8,4 +10,17 @@ namespace taichi::lang {
     {
         return std::make_unique<Kernel>(program, std::move(ir), name, autodiff_mode);
     }
+
+    namespace chirp
+    {
+        std::unique_ptr<IRBuilder::LoopGuard> get_loop_guard(IRBuilder &builder, Stmt *loop) {
+            return std::make_unique<IRBuilder::LoopGuard>(builder, loop);
+        }
+
+        //TODO!
+        GlobalPtrStmt *create_global_ptr(IRBuilder &builder, SNode *snode, const std::vector<Stmt *> &indices) {
+            return builder.insert(Stmt::make_typed<GlobalPtrStmt>(snode, indices));
+        }
+    } // namespace chirp
+    
 }
--- taichi/ir/shim.h
+++ taichi/ir/shim.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "taichi/program/kernel.h"
+#include "taichi/ir/ir_builder.h"
 
 namespace taichi::lang {
     std::unique_ptr<Kernel> new_kernel(Program &program,
          std::unique_ptr<IRNode> ir,
          const std::string &name = "",
          AutodiffMode autodiff_mode = AutodiffMode::kNone);
+    namespace chirp {
+        std::unique_ptr<IRBuilder::LoopGuard> get_loop_guard(IRBuilder &builder, Stmt *loop);
+        GlobalPtrStmt *create_global_ptr(SNode *snode, const std::vector<Stmt *> &indices);
+    }
 }
